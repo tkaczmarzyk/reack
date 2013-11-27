d5c96f56338abbb984fcbecabc8bec14b73c61eb
Tomasz Kaczmarzyk <tomasz+github@kaczmarzyk.net>
2013-11-27 10:57:14 +0100

    failing e2e test for calculation with time in different unit

--- test/e2e/calculation.js
+++ test/e2e/calculation.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('First E2E test', function() {
+describe('Receipt sum', function() {
 
 	beforeEach(function(){
 		browser().navigateTo('/');
@@ -12,5 +12,12 @@ describe('First E2E test', function() {
 		input('timeWorked').enter('12');
 		input('dailyWage').enter('10000');
 		expect(element('span.sum').text()).toEqual('120000');
+	});
+
+	it('should correctly interpret time provided in hours', function(){
+		browser().navigateTo('#/receipt');
+		input('timeWorked').enter('160h');
+		input('dailyWage').enter('10000');
+		expect(element('span.sum').text()).toEqual('200000');
 	});
 })

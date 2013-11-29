e0a2ed5acb225785153b0c3f92f46d527922b6c0
Tomasz Kaczmarzyk <tomasz+github@kaczmarzyk.net>
2013-11-29 11:04:44 +0100

    fixed tests

--- test/e2e/calculation.js
+++ test/e2e/calculation.js
@@ -11,13 +11,21 @@ describe('Receipt sum', function() {
 		browser().navigateTo('#/receipt');
 		input('timeWorked').enter('12');
 		input('dailyWage').enter('10000');
-		expect(element('span.sum').text()).toEqual('120000');
+		
+		var count = element('span.sum').count();
+		for (var i = 0; i < count; i++) {
+			expect(repeater('span.sum').row(i).text()).toEqual('120000 netto PLN');
+		}
 	});
 
 	it('should correctly interpret time provided in hours', function(){
 		browser().navigateTo('#/receipt');
 		input('timeWorked').enter('160h');
 		input('dailyWage').enter('10000');
-		expect(element('span.sum').text()).toEqual('200000');
+
+		var count = element('span.sum').count();
+		for (var i = 0; i < count; i++) {
+			expect(repeater('span.sum').row(i).text()).toEqual('200000 netto PLN');
+		}
 	});
 })
